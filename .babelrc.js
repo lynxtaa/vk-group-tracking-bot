65581ed4441cc2ede399697a1debd23e3dc20934
Alex Kondratyuk <lynxtaa@gmail.com>
2020-11-14 16:44:40 +0300

    fix: better coverage reporting

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,9 @@ module.exports = {
 	presets: ['@babel/preset-typescript'],
 	plugins: [
 		isTest && '@babel/plugin-transform-modules-commonjs',
+		'babel-plugin-transform-typescript-metadata',
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		['@babel/plugin-proposal-class-properties', { loose: true }],
 		'@babel/plugin-proposal-optional-chaining',
 	].filter(Boolean),
 }
