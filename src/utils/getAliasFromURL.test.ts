d84f895fea2a89b76f4d2c3950884b1ea2b6f635
Alex Kondratyuk <lynxtaa@gmail.com>
2021-07-22 21:34:16 +0300

    fix: support public-groups

--- src/utils/getAliasFromURL.test.ts
+++ src/utils/getAliasFromURL.test.ts
@@ -3,4 +3,6 @@ import { getAliasFromURL } from './getAliasFromURL'
 it('returns path from url', () => {
 	expect(getAliasFromURL('http://vk.com/club11233')).toBe('club11233')
 	expect(getAliasFromURL('https://m.vk.com/club11233')).toBe('club11233')
+	expect(getAliasFromURL('https://vk.com/public11233')).toBe('club11233')
+	expect(getAliasFromURL('https://vk.com/public-group')).toBe('public-group')
 })
